30e9ce544e24f912061c9b341861367e406d4525
Victor Gil <baskeboler@gmail.com>
2017-12-11 02:09:52 -0300

    travis ci

--- install-deps.sh
+++ install-deps.sh
@@ -2,6 +2,27 @@
 
 export CC=gcc-7
 export CXX=g++-7
+
+# gflags
+git clone https://github.com/gflags/gflags
+cd gflags
+cmake .
+make install
+cd ..
+
+# rocksdb
+sudo apt-get install -yq \
+    libsnappy-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    liblz4-dev \
+    libzstd-dev
+git clone https://github.com/facebook/rocksdb
+cd rocksdb
+make static_lib
+sudo make install
+
+# folly, wangle and proxygen
 git clone https://github.com/facebook/proxygen
 cd proxygen/proxygen
 ./deps.sh
